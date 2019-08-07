1bdd4830f3f3e95b842914495625553d2d032c5c
root <root@packstack-stein.localdomain>
2019-08-07 07:52:06 +0500

    added cloudkitty script

---
+++ cloudkitty_qs.sh
@@ -0,0 +1,33 @@
+mysql << EOF
+DROP DATABASE cloudkitty;
+CREATE DATABASE cloudkitty;
+GRANT ALL PRIVILEGES ON cloudkitty.* TO 'cloudkitty'@'localhost' IDENTIFIED BY 'cloudkitty';
+EOF
+
+mysql << EOF
+GRANT ALL PRIVILEGES ON cloudkitty.* TO 'cloudkitty'@'%' IDENTIFIED BY 'cloudkitty';
+EOF
+
+cloudkitty-storage-init
+
+cloudkitty-dbsync upgrade
+cloudkitty module enable hashmap 
+
+
+
+x=`cloudkitty hashmap service create ip.floating | grep ip.floating | awk '{print $4}' `
+
+cloudkitty hashmap mapping create 1 \
+ --service-id $x \
+ -t flat
+
+
+
+ y=`cloudkitty hashmap service create instance | grep instance | awk '{print $4}'`
+
+z=`cloudkitty hashmap field create $y flavor_id | grep flavor_id | awk '{print $4}'`
+
+ cloudkitty hashmap mapping create 1 \
+ --field-id $z \
+ --value 1 \
+ -t flat
