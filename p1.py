f0f58d829ed35c08bf44f217f50171a3910a81ad
root <k180960@nu.edu.pk>
2019-08-19 16:31:44 +0500

    refractor

--- p1.py
+++ p1.py
@@ -1,16 +1,18 @@
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
-auth = v3.Password(auth_url='http://192.168.24.18:5000/v3', user_id='8ceabf44a8e047718741f0fe7dcdfd0b',  password='babloo',
-                   project_id='f1b0fed0c651431c92e12a796ad3054e')
-sess = session.Session(auth=auth)
-keystone = client.Client(session=sess, include_metadata=True)
+#import pretty print
+import pprint
+p=pprint.PrettyPrinter(indent=4).pprint
+def pp(y):
+  return p(dir(y))
+#auth = v3.Password(auth_url='http://10.81.1.80:5000/v3', user_id='d6d7abde8b3541dd83c913cfa59672a9',  password='51d8ca1d90ec46dd',project_id='7f74bac0c5624988a0815d4aaf2ff049')
+#sess = session.Session(auth=auth)
+#keystone = client.Client(session=sess, include_metadata=True)
+keystone = client.Client(session= session.Session( auth=v3.Password(auth_url='http://10.81.1.80:5000/v3', username='admin',  password='51d8ca1d90ec46dd',project_id='7f74bac0c5624988a0815d4aaf2ff049',user_domain_id='default')) , include_metadata=True)
 resp = keystone.projects.list()
 
 
-#import pretty print
-import pprint
-pp=pprint.PrettyPrinter(indent=4).pprint
 
 
 
@@ -25,4 +27,12 @@ nova.servers.list()
 #gnocchi
 from gnocchiclient import client as gnocchi_client
 g=gnocchi_client.Client(1,session=sess)
+pp(dir(g))
+
+
+
+
+#cloudkitty
+from cloudkittyclient import client as cloudkitty_client
+cc=cloudkitty_client.Client(1,session=sess)
 pp(dir(g))
